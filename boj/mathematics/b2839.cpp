1a0ccfe50ea1003f2da555811d6f74a1b65a2192
DongHoon Yoo <nano@kakao.com>
2022-08-16 16:06:11 +0900

    Solved b2869

--- boj/mathematics/b2839.cpp
+++ boj/mathematics/b2839.cpp
@@ -2,3 +2,18 @@
 #include <stdio.h>
 #include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    unsigned short n;
+    scanf("%hu", &n);
+
+    if (n == 4 || n == 7)
+        printf("-1");
+    else if (n % 5 == 0)
+        printf("%hu", n / 5);
+    else
+        printf("%hu", n / 5 + 1 + ((n % 5) % 2 == 0));
+    
+    return 0;
+}
