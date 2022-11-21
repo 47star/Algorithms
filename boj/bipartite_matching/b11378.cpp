6a94e5a1492b095a25de1d54a6ec9715bd9f20a6
DongHoon Yoo <nano@kakao.com>
2022-11-21 20:56:52 +0900

    Solved b11378

---
+++ boj/bipartite_matching/b11378.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> vtx[1001];
+int slot[1001];
+bool done[1001];
+
+bool dfs(int x) {
+    done[x] = true;
+    int s = vtx[x].size();
+    for (int i = 0; i < s; i++) {
+        int p = vtx[x][i];
+
+        if (slot[p] == 0 || !done[slot[p]] && dfs(slot[p])) {
+            slot[p] = x;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(0);
+
+    int n, m, k;
+
+    scanf("%d %d %d", &n, &m, &k);
+    n++;
+
+    int i, j, c, w, r = 0;
+
+    for (i = 1; i < n; i++) {
+        scanf("%d", &c);
+        for (j = 0; j < c; j++) {
+            scanf("%d", &w);
+            vtx[i].push_back(w);
+        }
+    }
+
+    for (i = 1; i < n; i++) {
+        memset(done, false, sizeof(done));
+        if (dfs(i))
+            r++;
+    }
+
+    bool f = true;
+    while (0 < k && f) {
+        for (i = 1; i < n; i++) {
+            memset(done, false, sizeof(done));
+            if (dfs(i)) {
+                r++;
+                f = true;
+                --k;
+                break;
+            } else {
+                f = false;
+            }
+        }
+    }
+
+    printf("%d", r);
+
+    return 0;
+}
